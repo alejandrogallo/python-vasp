65707a2ab03b9fbf2e154823fbb5e4c767bac27b
Alejandro Gallo <aamsgallo@gmail.com>
2018-09-12 16:02:02 +0200

    Implement parse_vector

---
+++ tests/parser/test_regex.py
@@ -0,0 +1,17 @@
+from vasp.parser.regex import *
+
+
+def test_parse_vector():
+    assert(parse_vector("  1 2    4") == [1.0, 2.0, 4.0])
+    assert(parse_vector("1 2 4") == [1.0, 2.0, 4.0])
+    assert(parse_vector("1.342 2 4.2") == [1.342, 2.0, 4.2])
+    assert(parse_vector(" -1.342 2 4.2") == [-1.342, 2.0, 4.2])
+    assert(parse_vector(" -1.342 2E2 4.2") == [-1.342, 200.0, 4.2])
+    assert(parse_vector(" -1.342 2e+2 4.2") == [-1.342, 200.0, 4.2])
+    assert(parse_vector(" -1.342 2e-2 4.2") == [-1.342, 0.02, 4.2])
+    try:
+        assert(parse_vector("1.342 a 4.2") == [1.342, 2.0, 4.2])
+    except SyntaxError:
+        assert(True)
+    else:
+        assert(False)
--- vasp/parser/regex.py
+++ vasp/parser/regex.py
@@ -11,3 +11,19 @@ spin_component_header = re.compile(r"^spin component ([12])$")
 
 # This is for the outcar, every section seems to end by a long series of '-'
 end_section = re.compile(r'-' * 30)
+
+vector = re.compile(
+    r'^ *(\+?-?\d.?\d*E?\+?-?\d*) *'
+    r'(\+?-?\d.?\d*E?\+?-?\d*) *'
+    r'(\+?-?\d.?\d*E?\+?-?\d*) *$'
+)
+
+
+def parse_vector(line):
+    m = vector.match(line)
+    if not m:
+        raise SyntaxError(
+            'Line "{0}" does not seem to be a vector'.format(line)
+        )
+    else:
+        return [float(m.group(i + 1)) for i in range(3)]
