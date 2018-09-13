e439b4cd4a34dde95d89863e5c01a8346f159868
Alejandro Gallo <aamsgallo@gmail.com>
2018-09-13 11:54:40 +0200

    Add Chgcar parser

--- tests/parser/test_regex.py
+++ tests/parser/test_regex.py
@@ -25,3 +25,12 @@ def test_parse_vector():
         assert(False)
 
     assert(parse_vector(" -1 2 4", 3, int) == [-1, 2, 4])
+
+    assert(parse_vector(" -1 2 4", None, int) == [-1, 2, 4])
+
+    line = (
+        '13.567      12.386      9.4865      6.2601      3.8194      '
+        '2.4781      1.9825      1.9538      2.0889      2.1838    '
+    )
+    parsed = parse_vector(line, None, float)
+    assert(parsed[0] == 13.567)
